f5a32d8d7e28c7e6992b78c445d9bdf3ad3f80dd
urg0 <urgulerr@gmail.com>
2024-01-03 06:32:01 +0300

    styling updated.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+
 import MultiSelect from "./components/multi-select/MultiSelect";
+
+import Rick from "./assets/images/rick2.png";
 
 const App = () => {
   return (
     <div className="app-container">
+      <img src={Rick} alt="rick" className="rick" />
       <MultiSelect />
     </div>
   );
